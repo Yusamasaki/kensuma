ad72e2019af00b2dcd2b2a7931c09af82f4383c2
yusamasaki <masa.aw1208@gmail.com>
2022-01-21 12:17:03 +0900

    business_spec_brushup

--- spec/system/businesses_spec.rb
+++ spec/system/businesses_spec.rb
@@ -1,28 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'Businesses', type: :system do
-  let(:user) { create(:user) }
+  let(:user) { build(:user) }
 
-  let(:business) { create(:business) }
+  let(:business) { build(:business, user: user) }
 
-  describe '事業所関連' do
-    before(:each) do
-      user.skip_confirmation!
-      user.save!
-      visit new_user_session_path
-      fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: user.password
-      click_button 'ログイン'
+  describe '事業所CRUD' do
+    describe 'ログインするユーザーの事業所が登録されていない' do
+      before(:each) do
+        user.skip_confirmation!
+        user.save!
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'ログイン'
+      end
+
+      it '事業所新規作成' do
+        # フォーム内は現在defaultでデータが入っているためfill_inはスキップ
+        click_button '登録'
+        expect(page).to have_current_path users_dash_boards_path, ignore_query: true
+        expect(page).to have_content 'DashBoard'
+      end
     end
 
-    context '画面への推移が正常' do
-      it '事業所新規作成' do
-        # 事業所新規登録画面へ遷移
-        visit new_users_business_path
-        # new_users_businesses_pathへ遷移することを期待する
-        expect(page).to have_current_path new_users_business_path, ignore_query: true
-        # 遷移されたページに'事業所登録'の文字列があることを期待する
-        expect(page).to have_content '事業所登録'
+    describe 'ログインするユーザーの事業所が登録されている' do
+      before(:each) do
+        user.skip_confirmation!
+        user.save!
+        # 事業所登録
+        business.save!
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'ログイン'
+      end
+
+      it 'DashBoard画面が表示' do
+        expect(page).to have_current_path users_dash_boards_path, ignore_query: true
+        expect(page).to have_content 'DashBoard'
+      end
+
+      it '事業所編集画面の表示' do
+        visit edit_users_business_path
+        expect(page).to have_current_path edit_users_business_path, ignore_query: true
+        expect(page).to have_content '事業所編集'
+      end
+
+      it '事業所詳細画面の表示' do
+        visit users_business_path
+        expect(page).to have_current_path users_business_path, ignore_query: true
+        expect(page).to have_content '事業所詳細'
       end
     end
   end
